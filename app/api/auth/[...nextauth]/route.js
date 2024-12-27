d4b6f9b54b8b55979b4f13597d01face8feacb53
Muhammad Idrees <muhammadidr1122@gmail.com>
2024-12-27 10:22:00 -0800

    new commit

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,13 +1,8 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
-//import GitHubProvider from 'next-auth/providers/github';
-//import mongoose from 'mongoose';
- 
- 
 
 export const authoptions = NextAuth({
   providers: [
- 
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
@@ -19,15 +14,8 @@ export const authoptions = NextAuth({
     timeout: 10000, // 10 seconds timeout
   },
 
-
-  pages: {
-    signIn: '/auth/signin', // Custom sign-in page if needed
-    error: '/auth/error', // Error page if there's an issue
-  },
-
   secret: process.env.NEXTAUTH_SECRET, // Secure token signing
 });
 
-// Named export for GET and POST methods
 export const GET = authoptions;
 export const POST = authoptions;
